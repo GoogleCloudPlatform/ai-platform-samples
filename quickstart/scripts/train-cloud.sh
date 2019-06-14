8d480acb39c8101e94058807ae5276eabb32cd72
happyhuman <shahins@google.com>
2019-06-14 13:55:19 -0700

    Added quickstart files

---
+++ quickstart/scripts/train-cloud.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# This is the common setup.
+echo "Submitting an AI Platform job..."
+
+TIER="BASIC" # BASIC | BASIC_GPU | STANDARD_1 | PREMIUM_1
+
+export MODEL_NAME="quickstart" # change to your model name
+
+PACKAGE_PATH=./trainer # this can be a gcs location to a zipped and uploaded package
+export MODEL_DIR=gs://${BUCKET_NAME}/${MODEL_NAME}/model
+
+CURRENT_DATE=`date +%Y%m%d_%H%M%S`
+JOB_NAME=train_${MODEL_NAME}_${CURRENT_DATE}
+
+gcloud ai-platform jobs submit training ${JOB_NAME} \
+        --job-dir=${MODEL_DIR} \
+        --runtime-version=${TF_VERSION} \
+        --region=${REGION} \
+        --scale-tier=${TIER} \
+        --module-name=trainer.task \
+        --package-path=${PACKAGE_PATH}  \
+        --python-version=${PYTHON_VERSION} \
+        -- \
+	    ${MODEL_DIR}
+
+# Notes:
+# use --packages instead of --package-path if gcs location
+# add --reuse-job-dir to resume training
---
+++ quickstart/scripts/train-local.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+echo "Training local ML model"
+
+MODEL_NAME="quickstart" # Change to your model name, e.g. "estimator"
+
+PACKAGE_PATH=./trainer
+MODEL_DIR=./trained/${MODEL_NAME}
+mkdir -p ${MODEL_DIR}
+
+gcloud ai-platform local train \
+        --module-name=trainer.task \
+        --package-path=${PACKAGE_PATH} \
+        -- \
+        ${MODEL_DIR}
