12776d81c8d156105a9748fc40884310f8a7e244
nnegrey <nnegrey@google.com>
2019-07-23 11:24:37 -0600

    Add PyTorch GPU template

--- training/pytorch/structured/custom_containers/gpu/scripts/train-local.sh
+++ training/pytorch/structured/custom_containers/gpu/scripts/train-local.sh
@@ -13,55 +13,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-# This scripts performs cloud training for a PyTorch model.
-echo "Training cloud ML model"
+# This scripts performs local training for a PyTorch model.
+echo "Training local ML model"
 
 # IMAGE_REPO_NAME: the image will be stored on Cloud Container Registry
-IMAGE_REPO_NAME=pytorch_taxi_container
+IMAGE_REPO_NAME=pytorch_gpu_taxi_container
 
 # IMAGE_TAG: an easily identifiable tag for your docker image
-IMAGE_TAG=taxi_pytorch
+IMAGE_TAG=taxi_pytorch_gpu
 
 # IMAGE_URI: the complete URI location for Cloud Container Registry
 IMAGE_URI=gcr.io/${PROJECT_ID}/${IMAGE_REPO_NAME}:${IMAGE_TAG}
 
-# JOB_NAME: the name of your job running on AI Platform.
-JOB_NAME=custom_container_job_$(date +%Y%m%d_%H%M%S)
-
-# REGION: select a region from https://cloud.google.com/ml-engine/docs/regions
-# or use the default '`us-central1`'. The region is where the model will be deployed.
-REGION=us-central1
-
 # Build the docker image
 docker build -f Dockerfile -t ${IMAGE_URI} ./
 
-# Deploy the docker image to Cloud Container Registry
-docker push ${IMAGE_URI}
-
-# Submit your training job
-echo "Submitting the training job"
-
 # These variables are passed to the docker image
-JOB_DIR=gs://${BUCKET_ID}/models/base
 # Note: these files have already been copied over when the image was built
 TRAIN_FILES=taxi_trips_train.csv
 EVAL_FILES=taxi_trips_eval.csv
 
-gcloud beta ai-platform jobs submit training ${JOB_NAME} \
-    --region ${REGION} \
-    --master-image-uri ${IMAGE_URI} \
-    --scale-tier BASIC \
-    -- \
-    --train-files ${TRAIN_FILES} \
-    --eval-files ${EVAL_FILES} \
-    --num-epochs=10 \
-    --batch-size=100 \
-    --learning-rate=0.001 \
-    --job-dir=${JOB_DIR}
-
-# Stream the logs from the job
-gcloud ai-platform jobs stream-logs ${JOB_NAME}
-
-# Verify the model was exported
-echo "Verify the model was exported:"
-gsutil ls ${JOB_DIR}/model_*
+# Test your docker image locally
+echo "Running the Docker Image"
+docker run ${IMAGE_URI} \
+        --train-files ${TRAIN_FILES} \
+        --eval-files ${EVAL_FILES} \
+        --num-epochs=10 \
+        --batch-size=100 \
+        --learning-rate=0.001
