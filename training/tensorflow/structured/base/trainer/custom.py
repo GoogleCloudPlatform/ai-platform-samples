19e5822d4a3717918d8ecb2e58222bc826c3b784
Andrew Ferlitsch <aferlitsch@gmail.com>
2019-08-28 15:44:26 -0700

    Update custom.py

--- training/tensorflow/structured/base/trainer/custom.py
+++ training/tensorflow/structured/base/trainer/custom.py
@@ -18,3 +18,23 @@ import tensorflow as tf
 from . import featurizer
 from . import metadata
 
+
+def create(args, config):
+    """ Create a tf.keras Wide & Deep based on metadata.TASK_TYPE
+    Args:
+      args: experiment parameters.
+      config: tf.RunConfig object. Returns a Classifier or Regressor
+    """
+
+    wide_columns, deep_columns = featurizer.create_wide_and_deep_columns(args)
+    logging.info('Wide columns: {}'.format(wide_columns))
+    logging.info('Deep columns: {}'.format(deep_columns))
+
+    # Change the optimizers for the wide and deep parts of the model if you wish
+    linear_optimizer = tf.train.FtrlOptimizer(learning_rate=args.learning_rate)
+    # Use _update_optimizer to implement an adaptive learning rate
+
+    def predicate(): return _update_optimizer(args)
+
+    dnn_optimizer = predicate
+
