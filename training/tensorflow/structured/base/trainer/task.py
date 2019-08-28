8c83de2913ee45569273390ada40ef371fe13210
Andrew Ferlitsch <aferlitsch@gmail.com>
2019-08-28 15:30:03 -0700

    fix spelling, grammar, indentation and correcting text

--- training/tensorflow/structured/base/trainer/task.py
+++ training/tensorflow/structured/base/trainer/task.py
@@ -38,12 +38,12 @@ def get_args():
     # Data files arguments
     args_parser.add_argument(
         '--train-files',
-        help='GCS or local paths to training data',
+        help='GCS or local paths to training data.',
         nargs='+',
         required=True)
     args_parser.add_argument(
         '--eval-files',
-        help='GCS or local paths to evaluation data',
+        help='GCS or local paths to evaluation data.',
         nargs='+',
         required=True)
 
@@ -51,20 +51,20 @@ def get_args():
     args_parser.add_argument(
         '--train-steps',
         help="""
-      Steps to run the training job for.
-      If --num-epochs and --train-size are not specified, this must be.
-      Otherwise the training job will run indefinitely.
-      if --num-epochs and --train-size are specified,
-      then --train-steps will be: (train-size/train-batch-size) * num-epochs
-      """,
+        Steps to run the training job for.
+        If --num-epochs and --train-size are not specified, this must be specified;
+        otherwise the training job will run indefinitely.
+        if --num-epochs and --train-size are specified,
+        then --train-steps will default to: (train-size/train-batch-size) * num-epochs
+        """,
         default=0,
         type=int)
     args_parser.add_argument(
         '--eval-steps',
         help="""
-      Number of steps to run evaluation for at each checkpoint.',
-      Set to None to evaluate on the whole evaluation data.
-      """,
+        Number of steps to run evaluation for at each checkpoint.,
+        Set to None to evaluate on the whole evaluation data.
+        """,
         default=None,
         type=int)
     args_parser.add_argument(
@@ -74,7 +74,11 @@ def get_args():
         default=128)
     args_parser.add_argument(
         '--train-size',
-        help='Size of training set (instance count)',
+        help="""\
+        Size of the training data (instance count).
+        If both --train-size and --num-epochs are specified,
+        --train-steps will default to: (train-size/train-batch-size) * num-epochs.\
+        """,        
         type=int,
         default=None)
     args_parser.add_argument(
@@ -82,7 +86,7 @@ def get_args():
         help="""\
         Maximum number of training data epochs on which to train.
         If both --train-size and --num-epochs are specified,
-        --train-steps will be: (train-size/train-batch-size) * num-epochs.\
+        --train-steps will default to: (train-size/train-batch-size) * num-epochs.\
         """,
         default=100,
         type=int,
@@ -97,28 +101,28 @@ def get_args():
     args_parser.add_argument(
         '--embed-categorical-columns',
         help="""
-      If set to True, the categorical columns will be embedded
-      and used in the deep part og the model.
-      The embedding size = sqrt(vocab_size).
-      """,
+        If set to True, the categorical columns will be embedded
+        and used in the deep part of the model.
+        The embedding size = sqrt(vocab_size).
+        """,
         action='store_true',
         default=True,
     )
     args_parser.add_argument(
         '--use-indicator-columns',
         help="""
-      If set to True, the categorical columns will be encoded
-      as One-Hot indicators in the deep part of the model.
-      """,
+        If set to True, the categorical columns will be encoded
+        as One-Hot indicators in the deep part of the model.
+        """,
         action='store_true',
         default=False,
     )
     args_parser.add_argument(
         '--use-wide-columns',
         help="""
-      If set to True, the categorical columns will be used in the
-      wide part of the model.
-      """,
+        If set to True, the categorical columns will be used in the
+        wide part of the model.
+        """,
         action='store_true',
         default=False,
     )
@@ -132,39 +136,40 @@ def get_args():
     args_parser.add_argument(
         '--learning-rate-decay-factor',
         help="""
-      The factor by which the learning rate should decay by the end of the
-      training.
-      decayed_learning_rate = learning_rate * decay_rate ^ (global_step /
-      decay_steps).
-      If set to 1.0 (default), then no decay will occur.
-      If set to 0.5, then the learning rate should reach 0.5 of its original
-          value at the end of the training.
-      Note that decay_steps is set to train_steps.
-      """,
+        The factor by which the learning rate should decay by the end of the
+        training.
+        decayed_learning_rate = learning_rate * decay_rate ^ (global_step /
+        decay_steps).
+        If set to 1.0 (default), then no decay will occur.
+        If set to 0.5, then the learning rate should reach 0.5 of its original
+        value at the end of the training.
+        Note that decay_steps is set to train_steps.
+        """,
         default=1.0,
         type=float)
     args_parser.add_argument(
         '--hidden-units',
         help="""
-      Hidden layer sizes to use for DNN feature columns, provided in
-      comma-separated layers.
-      If --scale-factor > 0, then only the size of the first layer will be
-      used to compute
-      the sizes of subsequent layers.
-      """,
+        Hidden layer sizes to use for DNN feature columns, provided in
+        comma-separated layers.
+        If --scale-factor > 0, then only the size of the first layer will be
+        used to compute the sizes of subsequent layers.
+        """,
         default='30,30,30')
     args_parser.add_argument(
         '--layer-sizes-scale-factor',
         help="""
-      Determine how the size of the layers in the DNN decays.
-      If value = 0 then the provided --hidden-units will be taken as is
-      """,
+        Determine how the size of the layers in the DNN are scaled per successive layer.
+        If value = 0 then the provided --hidden-units will be the same per layer.
+        """,
         default=0.7,
         type=float)
     args_parser.add_argument(
         '--num-layers',
-        help='Number of layers in the DNN. If --scale-factor > 0, then this '
-             'parameter is ignored',
+        help="""
+        Number of layers in the DNN. If --layer-sizes-scale-factor > 0, then this
+        parameter is ignored.
+        """,
         default=4,
         type=int)
     args_parser.add_argument(
@@ -175,16 +180,16 @@ def get_args():
     # Saved model arguments
     args_parser.add_argument(
         '--job-dir',
-        help='GCS location to write checkpoints and export models',
+        help='GCS location to write checkpoints and export models.',
         required=True)
     args_parser.add_argument(
         '--reuse-job-dir',
         action='store_true',
         default=False,
         help="""
-      Flag to decide if the model checkpoint should be re-used from the job-dir.
-      If set to False then the job-dir will be deleted.
-      """)
+        Flag to decide if the model checkpoint should be re-used from the job-dir.
+        If set to False then the job-dir will be deleted.
+        """)
     args_parser.add_argument(
         '--serving-export-format',
         help='The input format of the exported serving SavedModel.',
