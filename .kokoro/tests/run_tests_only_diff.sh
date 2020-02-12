48bee610f8af408755e13e3b561d01494663deec
Gonzalo Gasca Meza <gogasca@google.com>
2020-02-11 22:45:45 -0800

    Refactor Kokoro tests

--- .kokoro/tests/run_tests_only_diff.sh
+++ .kokoro/tests/run_tests_only_diff.sh
@@ -1,4 +1,5 @@
-# Copyright 2019 Google LLC
+#!/usr/bin/env bash
+# Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,5 +12,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# Format: //devtools/kokoro/config/proto/build.proto
+mydir="${0%/*}"
+"$mydir"/run_tests.sh --only-diff
