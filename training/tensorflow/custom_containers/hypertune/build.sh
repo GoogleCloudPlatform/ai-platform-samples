b5451b51c64680ba0d36e3675c1e8e345cdf8cb8
Gonzalo Gasca Meza <gogasca@google.com>
2020-04-17 23:00:42 -0700

    Reformat code

--- training/tensorflow/custom_containers/hypertune/build.sh
+++ training/tensorflow/custom_containers/hypertune/build.sh
@@ -1,10 +1,27 @@
+#!/bin/bash
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+# Build Local Docker container.
+
 export PROJECT_ID=$(gcloud config list project --format "value(core.project)")
 export IMAGE_REPO_NAME=estimator-hypertune
 export IMAGE_TAG=latest
 export IMAGE_URI=gcr.io/$PROJECT_ID/$IMAGE_REPO_NAME:$IMAGE_TAG
 
-docker build -f Dockerfile -t $IMAGE_URI ./
+docker build -f Dockerfile -t "$IMAGE_URI" ./
 
 gcloud auth configure-docker
 
-docker push $IMAGE_URI:latest
+docker push "$IMAGE_URI":latest
