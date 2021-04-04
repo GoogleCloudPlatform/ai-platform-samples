411cdbdad387712e0730ee9821129b9eab3597c8
Ivan Cheung <ivans.mailbox@gmail.com>
2021-04-04 04:15:38 -0400

    Added cloud build notebook testing (#261)
    * Added cloud-build script
    
    * Removed Jupyter install
    
    test.ipynb
    
    Removed unused placeholder
    
    test
    
    Added gcloud config list test
    
    Removed print statements
    
    Fixed gsutil path
    
    Removed unneeded ipynb file
    
    Updated default zone for TPU, and modified tensor to int (#258)
    
    Co-authored-by: Crystal Gomes <thedriftofwords@users.noreply.github.com>
    
    Removed unneeded ipynb file
    
    Co-authored-by: Ivan Cheung <ivanmkc@google.com>

---
+++ .cloud-build/RemoveNoExecuteCells.py
@@ -0,0 +1,13 @@
+from nbconvert.preprocessors import Preprocessor
+
+
+class RemoveNoExecuteCells(Preprocessor):
+    def preprocess(self, notebook, resources=None):
+        executable_cells = []
+        for cell in notebook.cells:
+            if cell.metadata.get("tags"):
+                if "no_execute" in cell.metadata.get("tags"):
+                    continue
+            executable_cells.append(cell)
+        notebook.cells = executable_cells
+        return notebook, resources
