4b0fbc981ef6c3f43a1ec67afcc65de9b38256d9
Ivan Cheung <ivans.mailbox@gmail.com>
2021-04-10 14:07:47 -0400

    Tweaked linting job so it can be run by users locally. (#272)
    * Consolidated lint and format
    
    * Small tweaks
    
    * test
    
    * Check all return codes
    
    * Removed test file

--- .github/workflows/linter/run_linter.sh
+++ .github/workflows/linter/run_linter.sh
@@ -13,12 +13,39 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# `-e` enables the script to automatically fail when a command fails
+# This script automatically formats and lints all notebooks that have changed from the head of the master branch.
+#
+# Options:
+# -t: Test-mode. Only test if format and linting are required but make no changes to files.
+#
+# Returns:
+# This script will return 0 if linting was successful/unneeded and 1 if there were any errors.
+
+# `+e` enables the script to continue even when a command fails
+set +e
+
 # `-o pipefail` sets the exit code to the rightmost comment to exit with a non-zero
-set -eo pipefail
+set -o pipefail
 
 # Use RTN to return a non-zero value if the test fails.
-RTN=0
+RTN="0"
+
+is_test=false
+
+# Process all options supplied on the command line 
+while getopts 'tc' arg; do
+    case $arg in
+        't')
+            is_test=true
+            ;;
+        *)
+            echo "Unimplemented flag"
+            exit 1
+            ;;
+    esac
+done
+
+echo "Test mode: $is_test"
 
 # Only check notebooks in test folders modified in this pull request.
 # Note: Use process substitution to persist the data in the array
@@ -33,17 +60,64 @@ if [ ${#notebooks[@]} -gt 0 ]; then
     do    
         if [ -f "$notebook" ]; then
             echo "Checking notebook: ${notebook}"
-            echo "Running black..."
-            python3 -m nbqa black "$notebook" --check
-            echo "Running pyupgrade..."
-            python3 -m nbqa pyupgrade "$notebook"
-            echo "Running isort..."
-            python3 -m nbqa isort "$notebook" --check
-            echo "Running flake8..."
-            python3 -m nbqa flake8 "$notebook" --show-source --ignore=W391,E501,F821,E402,F404
 
-            NOTEBOOK_RTN=$?
-            echo "Notebook finished with return code = $NOTEBOOK_RTN"
+            if [ "$is_test" = true ] ; then
+                echo "Running nbfmt..."
+                python3 -m tensorflow_docs.tools.nbfmt --test "$notebook"
+                NBFMT_RTN=$?
+                echo "Running black..."
+                python3 -m nbqa black "$notebook" --check
+                BLACK_RTN=$?
+                echo "Running pyupgrade..."
+                python3 -m nbqa pyupgrade "$notebook"
+                PYUPGRADE_RTN=$?
+                echo "Running isort..."
+                python3 -m nbqa isort "$notebook" --check
+                ISORT_RTN=$?
+                echo "Running flake8..."
+                python3 -m nbqa flake8 "$notebook" --show-source --ignore=W391,E501,F821,E402,F404
+                FLAKE8_RTN=$?
+            else
+                echo "Running nbfmt..."
+                python3 -m tensorflow_docs.tools.nbfmt "$notebook"
+                NBFMT_RTN=$?
+                echo "Running black..."
+                python3 -m nbqa black "$notebook" --nbqa-mutate
+                BLACK_RTN=$?
+                echo "Running pyupgrade..."
+                python3 -m nbqa pyupgrade "$notebook" --nbqa-mutate
+                PYUPGRADE_RTN=$?
+                echo "Running isort..."
+                python3 -m nbqa isort "$notebook" --nbqa-mutate
+                ISORT_RTN=$?
+                echo "Running flake8..."
+                python3 -m nbqa flake8 "$notebook" --show-source --ignore=W391,E501,F821,E402,F404 --nbqa-mutate
+                FLAKE8_RTN=$?
+            fi
+
+            NOTEBOOK_RTN="0"
+
+            if [ "$NBFMT_RTN" != "0" ]; then
+                NOTEBOOK_RTN="$NBFMT_RTN"
+            fi
+            
+            if [ "$BLACK_RTN" != "0" ]; then
+                NOTEBOOK_RTN="$BLACK_RTN"
+            fi
+
+            if [ "$PYUPGRADE_RTN" != "0" ]; then
+                NOTEBOOK_RTN="$PYUPGRADE_RTN"
+            fi
+
+            if [ "$ISORT_RTN" != "0" ]; then
+                NOTEBOOK_RTN="$ISORT_RTN"
+            fi
+
+            if [ "$FLAKE8_RTN" != "0" ]; then
+                NOTEBOOK_RTN="$FLAKE8_RTN"
+            fi
+
+            echo "Notebook link finished with return code = $NOTEBOOK_RTN"
             echo ""
             if [ "$NOTEBOOK_RTN" != "0" ]
             then                                
