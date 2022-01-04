1ed27bf34c9e970f089845a537a6ab2b2d01a01d
Karl Weinmeister <11586922+kweinmeister@users.noreply.github.com>
2022-01-04 14:03:59 -0600

    chore: Remove unsupported notebook tools (#564)

--- notebooks/tools/nova-jupyterlab-extensions/lib/menu_ext.d.ts
+++
@@ -1,7 +0,0 @@
-import { JupyterLabPlugin } from '@jupyterlab/application';
-import '../style/index.css';
-/**
- * Initialization data for the jlab_xkcd extension.
- */
-declare const extension: JupyterLabPlugin<void>;
-export default extension;
